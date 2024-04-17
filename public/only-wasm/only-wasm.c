15285bc85ccfcab8c34e182391d335788fa41e5a
Jeremy Ellis <keyfreemusic@gmail.com>
2024-04-17 11:24:15 -0700

    Update only-wasm.c

--- public/only-wasm/only-wasm.c
+++ public/only-wasm/only-wasm.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <emscripten.h>
 
+EMSCRIPTEN_KEEPALIVE
 void hello() {
     printf("Hello, World!\n");
 }
 
+EMSCRIPTEN_KEEPALIVE
 int main() {
     // Empty main function
     return 0;
