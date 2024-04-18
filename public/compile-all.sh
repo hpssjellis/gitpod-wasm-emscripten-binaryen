8b3f88542a0ff4c9539308c01eb7cb2d07ec1a42
Jeremy Ellis <keyfreemusic@gmail.com>
2024-04-17 23:52:47 -0700

    Update compile-all.sh

--- public/compile-all.sh
+++ public/compile-all.sh
@@ -24,7 +24,7 @@ emcc only-wasm.c -s STANDALONE_WASM -s EXPORTED_FUNCTIONS='["_hello"]' -o only-w
 cd ..
 
 cd structs-if-for
-emcc structs-if-for.c -o structs-if-for.js -s EXPORTED_FUNCTIONS='["_printPerson", "_classifyGrade", "_sumOfNumbers", "_countToN", "_malloc","_free"]' -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]'
+emcc structs-if-for.c -o structs-if-for.js -s EXPORTED_FUNCTIONS='["_printPerson", "_classifyGrade", "_sumOfNumbers", "_countToN", "_malloc","_free"]' -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap","setValue","stringToUTF8"]'
 cd ..
 
 
