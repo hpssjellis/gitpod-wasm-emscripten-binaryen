21bc5373ca67639d4476891c238856475c2f8f40
Jeremy Ellis <keyfreemusic@gmail.com>
2024-04-17 21:58:58 -0700

    Create compile-all.sh

---
+++ public/compile-all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+# Auto compiles all the c and c++ files
+
+cd foobar
+emcc foobar.c -o foobar.js -s EXPORTED_FUNCTIONS='["_main", "_foo", "_bar"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]'
+cd ..
+
+cd hello-cpp
+em++ hello-cpp.cpp -o hello-cpp.js
+cd ..
+
+cd hello_world
+emcc hello_world.c -o hello_world.js -s EXPORTED_FUNCTIONS='["_hello"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]'
+cd ..
+
+cd math_string_array
+emcc math_string_array.c -o math_string_array.js -s EXPORTED_FUNCTIONS='["_add", "_subtract", "_get_length", "_sum_array"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "_malloc", "_free"]'
+cd ..
+
+cd only-wasm
+emcc only-wasm.c -s STANDALONE_WASM -s EXPORTED_FUNCTIONS='["_hello"]' -o only-wasm.wasm
+cd ..
+
+cd structs-if-for
+emcc structs-if-for.c -o structs-if-for.js -s EXPORTED_FUNCTIONS='["_printPerson", "_classifyGrade", "_sumOfNumbers", "_countToN"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap","_malloc","_free","_printPerson"]'
+cd ..
+
+
+
+
+
+
+
+
+
